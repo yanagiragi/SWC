35285c343d8c5fb37cb32cf013a692d54080dc6f
吳孟宏 <wueric0622@gmail.com>
2017-11-25 16:09:13 +0800

    Add UI Manger & SlimeBehaviourManger

---
+++ Assets/Scripts/UIManger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManger : ManagerBase<UIManger>
+{
+
+    [SerializeField] int blood;
+    [SerializeField] int Satiation;
+    [SerializeField] int FoodAmount;
+    [SerializeField] Image bloodBar;
+    [SerializeField] Image satiationBar;
+    [SerializeField] Text FoodInfo;
+
+    void UpdateAtStep()
+    {
+        UpdateBloodBar();
+        UpdateSatiation();
+        UpdateFoodInfo();
+    }
+
+    public void UpdateBloodBar()
+    {
+        bloodBar.fillAmount = blood / 100.0f;
+    }
+
+	public void UpdateSatiation()
+	{
+        satiationBar.fillAmount = Satiation / 100.0f;
+    }
+
+	public void UpdateFoodInfo()
+	{
+        FoodInfo.text = "食物數量 : " + FoodAmount;
+    }
+}
