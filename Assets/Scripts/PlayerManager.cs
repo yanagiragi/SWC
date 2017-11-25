75284ba82fc3a5df1fc4d3d7413c9cc6da9c5d1d
yanagiragi <yanagiragi@users.noreply.github.com>
2017-11-26 04:48:20 +0800

    Finished Oil

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -328,7 +328,8 @@ public class PlayerManager : ManagerBase<PlayerManager> {
             bool isConflictWithYogurt = (yogurtInstance.transform.position.x != -1 && (yogurtInstance.transform.position - _nextPos).magnitude < 0.01f);
 			DungeonMapData _data = DungeonManager.GetMapData ((int)_nextPos.x, (int)_nextPos.z);
 
-            if (_data.cubeData.canThrough && !isConflictWithYogurt) {
+            if (_data.cubeData.canThrough && !isConflictWithYogurt)
+            {
                 if ((_data.itemData.canFuse) && (slimeMode != Item.ItemType.empty)) {
                     Item.ItemType resultType = CheckFuse(slimeMode, _data.itemType);
                     if ((int)resultType > (int)Item.ItemType.poison) {
@@ -340,13 +341,22 @@ public class PlayerManager : ManagerBase<PlayerManager> {
                     StepManager.InvokeStep();
                 }
 
+                if (_data.cubeType == E_DUNGEON_CUBE_TYPE.WATER)
+                {
+                    SlimeBehaviourManger.instance.GetNextStep(_data);
+
+                    SlimeBehaviourManger.instance.WalkOnWater();
+                }
+
             } else if (_data.cubeType == E_DUNGEON_CUBE_TYPE.EARTH && slimeMode == Item.ItemType.acid)
             {
                 SlimeBehaviourManger.instance.GetNextStep(_data);
 
                 SlimeBehaviourManger.instance.AcidMeltWall();
 
-            } else {
+            }
+            else
+            {
                 Rotate();
             }
         }
