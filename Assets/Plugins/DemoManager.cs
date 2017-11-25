72d065df705c7ee1bd59d5413d97c4de8bd65b80
Odls <alexeatapple@yahoo.com.tw>
2017-11-25 13:59:29 +0800

    ManagerBase 範例

---
+++ Assets/Plugins/DemoManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoManager : ManagerBase<DemoManager> {
+	int privateA;
+	public static int A{
+		get{
+			return instance.privateA;
+		}
+	}
+	public int B;
+
+	public static void FunA(){
+
+	}
+
+	public void FunB(){
+
+	}
+
+	void Start () {
+		//其他腳本使用：
+		Debug.Log(DemoManager.A);
+		Debug.Log(DemoManager.instance.B);
+		DemoManager.FunA ();
+		DemoManager.instance.FunB ();
+	}
+	void Update () {
+		
+	}
+}
