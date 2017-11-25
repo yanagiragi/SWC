913a86308bd267fc708d4171d54a3a448ec60708
吳孟宏 <wueric0622@gmail.com>
2017-11-25 20:57:53 +0800

    Add CameraMenger

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : ManagerBase<CameraManager> {
+    public Vector3 Offset;
+    void Update () {
+        Camera.main.gameObject.transform.position = PlayerManager.instance.playerInstance.transform.position + Offset;
+    }
+}
