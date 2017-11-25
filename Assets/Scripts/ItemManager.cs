66487e203a55ffd31de6c950053618963b0f1683
yanagiragi <yanagiragi@users.noreply.github.com>
2017-11-25 15:31:16 +0800

    update ItemManager

--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -5,38 +5,52 @@ using UnityEngine;
 public class ItemManager : ManagerBase<ItemManager> {
 
     [ReorderableList]
-    private List<Item> items = new List<Item>();
+    public List<Item> PlayerItemList = new List<Item>();
+
+    [ReorderableList]
+    public List<Item> MapItemList = new List<Item>();
 
     private Dictionary<Item.ItemType, Item> itemTypeMap = new Dictionary<Item.ItemType, Item>();
 
     public ItemManager()
     {
         // enum order: empty, milk, oil, butter, acid, yogurt, poison, food1, food2, food3
-        items.Add(new Item(Item.ItemType.empty, 0, false, false));
-        items.Add(new Item(Item.ItemType.milk, 0, true, true));
-        items.Add(new Item(Item.ItemType.oil, 0, true, true));
-        items.Add(new Item(Item.ItemType.butter, 0, false, true));
-        items.Add(new Item(Item.ItemType.acid, 0, true, true));
-        items.Add(new Item(Item.ItemType.yogurt, 0, false, true));
-        items.Add(new Item(Item.ItemType.poison, 0, false, true));
-        items.Add(new Item(Item.ItemType.food1, 0, false, false));
-        items.Add(new Item(Item.ItemType.food2, 0, false, false));
-        items.Add(new Item(Item.ItemType.food3, 0, false, false));
+        PlayerItemList.Add(new Item(Item.ItemType.empty, 0, false, false));
+        PlayerItemList.Add(new Item(Item.ItemType.milk, 0, true, true));
+        PlayerItemList.Add(new Item(Item.ItemType.oil, 0, true, true));
+        PlayerItemList.Add(new Item(Item.ItemType.butter, 0, false, true));
+        PlayerItemList.Add(new Item(Item.ItemType.acid, 0, true, true));
+        PlayerItemList.Add(new Item(Item.ItemType.yogurt, 0, false, true));
+        PlayerItemList.Add(new Item(Item.ItemType.poison, 0, false, true));
+        PlayerItemList.Add(new Item(Item.ItemType.food1, 0, false, false));
+        PlayerItemList.Add(new Item(Item.ItemType.food2, 0, false, false));
+        PlayerItemList.Add(new Item(Item.ItemType.food3, 0, false, false));
 
-        itemTypeMap.Add(Item.ItemType.empty, items[0]);
-        itemTypeMap.Add(Item.ItemType.milk, items[1]);
-        itemTypeMap.Add(Item.ItemType.oil, items[2]);
-        itemTypeMap.Add(Item.ItemType.butter, items[3]);
-        itemTypeMap.Add(Item.ItemType.acid, items[4]);
-        itemTypeMap.Add(Item.ItemType.yogurt, items[5]);
-        itemTypeMap.Add(Item.ItemType.poison, items[6]);
-        itemTypeMap.Add(Item.ItemType.food1, items[7]);
-        itemTypeMap.Add(Item.ItemType.food2, items[8]);
-        itemTypeMap.Add(Item.ItemType.food3, items[9]);
+        MapItemList.Add(new Item(Item.ItemType.empty, 0, false, false));
+        MapItemList.Add(new Item(Item.ItemType.milk, 0, true, true));
+        MapItemList.Add(new Item(Item.ItemType.oil, 0, true, true));
+        MapItemList.Add(new Item(Item.ItemType.butter, 0, false, true));
+        MapItemList.Add(new Item(Item.ItemType.acid, 0, true, true));
+        MapItemList.Add(new Item(Item.ItemType.yogurt, 0, false, true));
+        MapItemList.Add(new Item(Item.ItemType.poison, 0, false, true));
+        MapItemList.Add(new Item(Item.ItemType.food1, 0, false, false));
+        MapItemList.Add(new Item(Item.ItemType.food2, 0, false, false));
+        MapItemList.Add(new Item(Item.ItemType.food3, 0, false, false));
+
+        itemTypeMap.Add(Item.ItemType.empty, PlayerItemList[0]);
+        itemTypeMap.Add(Item.ItemType.milk, PlayerItemList[1]);
+        itemTypeMap.Add(Item.ItemType.oil, PlayerItemList[2]);
+        itemTypeMap.Add(Item.ItemType.butter, PlayerItemList[3]);
+        itemTypeMap.Add(Item.ItemType.acid, PlayerItemList[4]);
+        itemTypeMap.Add(Item.ItemType.yogurt, PlayerItemList[5]);
+        itemTypeMap.Add(Item.ItemType.poison, PlayerItemList[6]);
+        itemTypeMap.Add(Item.ItemType.food1, PlayerItemList[7]);
+        itemTypeMap.Add(Item.ItemType.food2, PlayerItemList[8]);
+        itemTypeMap.Add(Item.ItemType.food3, PlayerItemList[9]);
         
     }
 
-    bool FuseItem(Item item1, Item item2)
+    bool Fuse(Item item1, Item item2)
     {
         bool isSuccess = false;
 
